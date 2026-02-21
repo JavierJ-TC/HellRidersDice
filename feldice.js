c73bc15f3f5b562752758ac95cd43d4eee84f43d
Javier J. <jimenezgandiajavier@gmail.com>
2026-02-21 04:42:37 +0100

    d10 fel

--- feldice.js
+++ feldice.js
@@ -1,6 +1,12 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addSystem({id: "feldice", name: "Fel Dice"}, false);
 	dice3d.addDicePreset({
+		type: "d10",
+		labels: "",
+		modelFile: "modules/hellridersdice/fel_d10.glb",
+		system: "feldice"
+	});
+	dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
 		modelFile: "modules/hellridersdice/fel_d20.glb",
