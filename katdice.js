2ebc37b4d1910e9420f6a712c5b47b6f3d1bfdad
Javier J. <jimenezgandiajavier@gmail.com>
2026-02-21 03:02:41 +0100

    Kat d8

--- katdice.js
+++ katdice.js
@@ -1,9 +1,16 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addSystem({id: "katdice", name: "Kat Dice"}, false);
 	dice3d.addDicePreset({
+		type: "d8",
+		labels: "",
+		modelFile: "modules/hellridersdice/kat_d8.glb",
+		system: "katdice"
+	});
+	dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
 		modelFile: "modules/hellridersdice/kat_d20.glb",
 		system: "katdice"
 	});
+		
 });
